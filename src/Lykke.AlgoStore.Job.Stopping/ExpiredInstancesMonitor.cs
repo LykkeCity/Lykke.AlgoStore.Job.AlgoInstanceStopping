73763bccba5f6ceeffff4992176fdcac4baf4d6f
Filip Lyubenov <filip.lyubenov@primeholding.com>
2018-06-28 15:17:01 +0300

    Adde logic for aut deletion of expired instances

---
+++ src/Lykke.AlgoStore.Job.Stopping/ExpiredInstancesMonitor.cs
@@ -0,0 +1,120 @@
+﻿using Common.Log;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Enumerators;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Models;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Repositories;
+using Lykke.AlgoStore.Job.Stopping.Settings.JobSettings;
+using Lykke.AlgoStore.KubernetesClient;
+using Lykke.AlgoStore.KubernetesClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lykke.AlgoStore.Job.Stopping
+{
+    public class ExpiredInstancesMonitor
+    {
+        private readonly IAlgoClientInstanceRepository _algoClientInstanceRepository;
+        private readonly IKubernetesApiClient _kubernetesApiClient;
+        private readonly ExpiredInstancesMonitorSettings _settings;
+        private const string _loggingContext = "Check for expired instances";
+        private readonly ILog _log;
+
+        public ExpiredInstancesMonitor(
+            IAlgoClientInstanceRepository algoClientInstanceRepository,
+            IKubernetesApiClient kubernetesApiClient,
+            ExpiredInstancesMonitorSettings settings,
+            ILog log)
+        {
+            _algoClientInstanceRepository = algoClientInstanceRepository;
+            _kubernetesApiClient = kubernetesApiClient;
+            _settings = settings;
+            _log = log;
+        }
+
+        public async Task StartAsync()
+        {
+            await ExecuteEvery();
+        }
+
+        private async Task ExecuteEvery()
+        {
+            await Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var expiredInstances = await GetExpiredAlgoInstancesAsync();
+
+                    await TryStopExpiredInstances(expiredInstances);
+
+                    await Task.Delay(TimeSpan.FromSeconds(_settings.CheckIntervalInSeconds));
+                }
+            });
+        }
+
+        public async Task TryStopExpiredInstances(List<AlgoInstanceStoppingData> expiredInstances)
+        {
+            foreach (var instance in expiredInstances)
+            {
+                var instancePod = await GetInstancePodAsync(instance.InstanceId);
+                if (instancePod == null)
+                {
+                    await _log.WriteWarningAsync(nameof(ExpiredInstancesMonitor), _loggingContext, $"Instance {instance.InstanceId} of client id {instance.ClientId} is marked as started in db, but its pod was not found in kubernetis.");
+                    continue;
+                }
+                var deleted = await DeleteInstancePodAsync(instance, instancePod);
+                if (deleted)
+                {
+                    await MarkInstanceAsStoppedInDbAsync(instance);
+                }
+            }
+        }
+
+        public async Task<List<AlgoInstanceStoppingData>> GetExpiredAlgoInstancesAsync()
+        {
+            var instacnes = (await _algoClientInstanceRepository.GetAllAlgoInstancesPastEndDate(DateTime.UtcNow)).ToList();
+            return instacnes.Where(i=>i.AlgoInstanceStatus == AlgoInstanceStatus.Started).ToList();     
+        }
+
+        public async Task<Iok8skubernetespkgapiv1Pod> GetInstancePodAsync(string instanceId)
+        {
+            var pods = await _kubernetesApiClient.ListPodsByInstanceIdAsync(instanceId);
+
+            if (pods == null || pods.Count() == 0 || pods[0] == null)
+                return null;
+
+            return pods[0];
+        }
+
+        public async Task<bool> DeleteInstancePodAsync(AlgoInstanceStoppingData stoppingInstance, Iok8skubernetespkgapiv1Pod instancePod)
+        {
+            var deleted = await _kubernetesApiClient.DeleteAsync(stoppingInstance.InstanceId, instancePod.Metadata.NamespaceProperty);
+            if (deleted)
+            {              
+                await _log.WriteInfoAsync(nameof(ExpiredInstancesMonitor), _loggingContext, $"Successfully stopped instance pod for instance id {stoppingInstance.InstanceId} of client id {stoppingInstance.ClientId}");
+                return true;
+            }
+            else
+            {
+                await _log.WriteWarningAsync(nameof(ExpiredInstancesMonitor), _loggingContext, $"Could not stop pod in kubernetis for expired instance id {stoppingInstance.InstanceId} of client id {stoppingInstance.ClientId}");
+                return false;
+            }
+        }
+
+        public async Task<bool> MarkInstanceAsStoppedInDbAsync(AlgoInstanceStoppingData stoppingInstance)
+        {
+            var instance = await _algoClientInstanceRepository.GetAlgoInstanceDataByClientIdAsync(stoppingInstance.ClientId, stoppingInstance.InstanceId);
+            if(instance == null || instance.InstanceId==null || instance.AlgoId == null)
+            {
+                await _log.WriteWarningAsync(nameof(ExpiredInstancesMonitor), _loggingContext, $"Instance id {stoppingInstance.InstanceId} of client id {stoppingInstance.ClientId} not found in db and cannot be marked as stopped.");
+                return false;
+            }
+            instance.AlgoInstanceStopDate = DateTime.UtcNow;
+            instance.AlgoInstanceStatus = AlgoInstanceStatus.Stopped;
+            await _algoClientInstanceRepository.SaveAlgoInstanceDataAsync(instance);
+            await _log.WriteInfoAsync(nameof(ExpiredInstancesMonitor), _loggingContext, $"Successfully stopped instance pod for instance id {instance.InstanceId} of client id {instance.ClientId}");
+            return true;
+        }
+    }
+}
---
+++ tests/Lykke.AlgoStore.Job.Stopping.Tests/ExpiredInstancesMonitorTests.cs
@@ -0,0 +1,178 @@
+﻿using Common.Log;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Enumerators;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Models;
+using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Repositories;
+using Lykke.AlgoStore.Job.Stopping.Settings.JobSettings;
+using Lykke.AlgoStore.KubernetesClient;
+using Lykke.AlgoStore.KubernetesClient.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lykke.AlgoStore.Job.Stopping.Tests
+{
+    [TestFixture]
+    public class ExpiredInstancesMonitorTests
+    {
+        private const string InstanceId1 = "I1";
+        private const string ClientId1 = "C1";
+        private const string PodNamespace = "TESTNS";
+        private ExpiredInstancesMonitor defaultMonitorMock;
+        private IAlgoClientInstanceRepository defaultRepoMock;
+        private IKubernetesApiClient defaultKuberClient;
+
+        [SetUp]
+        public void SetUp()
+        {
+            defaultRepoMock = GetAlgoClientInstanceRepositoryMock();
+            defaultKuberClient = GetKubernetesApiClientMock();
+            defaultMonitorMock = new ExpiredInstancesMonitor(defaultRepoMock, defaultKuberClient, GetMockSettings(), GetMockLog());
+        }
+
+        [Test]
+        public void GetExpiredAlgoInstancesAsync_ExpectTwo_Test()
+        {
+            var startedAndExpiredInstancesInDb = defaultMonitorMock.GetExpiredAlgoInstancesAsync().Result;
+            Assert.IsTrue(startedAndExpiredInstancesInDb.Count() == 2);
+        }
+
+        [Test]
+        public void GetInstancePodAsync_ExpectOne_Test()
+        {
+            var pod = defaultMonitorMock.GetInstancePodAsync(InstanceId1).Result;
+            Assert.IsTrue(pod!=null);
+        }
+        [Test]
+        public void GetInstancePodAsync_ExpectNone_Test()
+        {
+            var pod = defaultMonitorMock.GetInstancePodAsync("non-existing").Result;
+            Assert.IsTrue(pod == null);
+        }
+
+        [Test]
+        public void DeleteInstancePodAsync_True_Test()
+        {
+            var stoppingInstance = new AlgoInstanceStoppingData { InstanceId = InstanceId1 };
+            var instancePod = new Iok8skubernetespkgapiv1Pod { Metadata = new Iok8sapimachinerypkgapismetav1ObjectMeta { NamespaceProperty = PodNamespace } };
+
+            var result = defaultMonitorMock.DeleteInstancePodAsync(stoppingInstance, instancePod).Result;
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void DeleteInstancePodAsync_False_Test()
+        {
+            var stoppingInstance = new AlgoInstanceStoppingData { InstanceId = "non-existing" };
+            var instancePod = new Iok8skubernetespkgapiv1Pod { Metadata = new Iok8sapimachinerypkgapismetav1ObjectMeta { NamespaceProperty = "non-existing" } };
+
+            var result = defaultMonitorMock.DeleteInstancePodAsync(stoppingInstance, instancePod).Result;
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void MarkInstanceAsStoppedInDbAsync_True_Test()
+        {
+            var stoppingInstance = new AlgoInstanceStoppingData { InstanceId = InstanceId1, ClientId = ClientId1 };
+            
+            var result = defaultMonitorMock.MarkInstanceAsStoppedInDbAsync(stoppingInstance).Result;
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void MarkInstanceAsStoppedInDbAsync_False_Test()
+        {
+            var result = defaultMonitorMock.MarkInstanceAsStoppedInDbAsync(new AlgoInstanceStoppingData()).Result;
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task TryStopExpiredInstances_Test()
+        {
+            var expiredInstances = GetMockListOfExpiredInstances().ToList();
+            var task = defaultMonitorMock.TryStopExpiredInstances(expiredInstances);
+            await task;
+            Assert.IsTrue(task.IsCompleted == true);
+        }
+
+        private ILog GetMockLog()
+        {
+            return new Mock<ILog>().Object;
+        }
+        private ExpiredInstancesMonitorSettings GetMockSettings()
+        {
+            return new Mock<ExpiredInstancesMonitorSettings>().Object;
+        }        
+
+        private IKubernetesApiClient GetKubernetesApiClientMock()
+        {
+            var kubernetesApiClient = new Mock<IKubernetesApiClient>();
+
+            kubernetesApiClient.Setup(a => a.ListPodsByInstanceIdAsync(It.IsAny<string>()))
+                 .Returns<string>((instanceId) =>
+                 {
+                     var allPods = GetMockListOfKubernetisPods().ToList();
+                     var found = allPods.FirstOrDefault(p => p.Metadata.Name == instanceId);
+                     IList<Iok8skubernetespkgapiv1Pod> test = found!=null ? new List<Iok8skubernetespkgapiv1Pod> { found } : new List<Iok8skubernetespkgapiv1Pod>();
+                     return Task.FromResult(test);
+                 });
+
+            kubernetesApiClient.Setup(a=>a.DeleteAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns<string, string>((instanceId, nameSpace) =>
+                {
+                    if (instanceId == InstanceId1 && nameSpace == PodNamespace)
+                        return Task.FromResult(true);
+                    return Task.FromResult(false);
+                });         
+
+            return kubernetesApiClient.Object;
+        }
+
+
+
+        private IAlgoClientInstanceRepository GetAlgoClientInstanceRepositoryMock()
+        {
+            var algoClientInstanceRepository = new Mock<IAlgoClientInstanceRepository>();
+
+            algoClientInstanceRepository.Setup(a => a.GetAllAlgoInstancesPastEndDate(It.IsAny<DateTime>()))
+                .Returns(Task.FromResult(GetMockListOfExpiredInstances()));
+
+
+            algoClientInstanceRepository.Setup(a => a.GetAlgoInstanceDataByClientIdAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns<string, string>((clientId, instanceId) =>
+                {
+                    var instances = GetMockListOfExpiredInstances().ToList();
+                    var found = instances.FirstOrDefault(i => i.ClientId == clientId && i.InstanceId == instanceId);
+                    return Task.FromResult(found ==null ? default(AlgoClientInstanceData) : new AlgoClientInstanceData { ClientId = found.ClientId, InstanceId = found.InstanceId, AlgoInstanceStatus = found.AlgoInstanceStatus });
+                });
+
+
+            algoClientInstanceRepository.Setup(a => a.SaveAlgoInstanceDataAsync(It.IsAny<AlgoClientInstanceData>()))
+                .Returns(Task.CompletedTask);
+
+
+            return algoClientInstanceRepository.Object;
+        }
+
+        private IEnumerable<AlgoInstanceStoppingData> GetMockListOfExpiredInstances()
+        {
+            return new List<AlgoInstanceStoppingData>
+            {
+                new AlgoInstanceStoppingData { AlgoId = "A1", AlgoInstanceStatus = CSharp.AlgoTemplate.Models.Enumerators.AlgoInstanceStatus.Started, ClientId = "C1", EndOnDateTicks = "636659723400000000", InstanceId = InstanceId1 },
+                new AlgoInstanceStoppingData { AlgoId = "A2", AlgoInstanceStatus = CSharp.AlgoTemplate.Models.Enumerators.AlgoInstanceStatus.Started, ClientId = "C2", EndOnDateTicks = "636665692200000000", InstanceId = "I2" },
+                new AlgoInstanceStoppingData { AlgoId = "A3", AlgoInstanceStatus = CSharp.AlgoTemplate.Models.Enumerators.AlgoInstanceStatus.Stopped, ClientId = "C3", EndOnDateTicks = "636665692200000000", InstanceId = "I3" },
+                new AlgoInstanceStoppingData { AlgoId = "A4", AlgoInstanceStatus = CSharp.AlgoTemplate.Models.Enumerators.AlgoInstanceStatus.Deploying, ClientId = "C4", EndOnDateTicks = "636665692200000000", InstanceId = "I4" }
+            };
+        }
+
+        private IEnumerable<Iok8skubernetespkgapiv1Pod> GetMockListOfKubernetisPods()
+        {
+            return new List<Iok8skubernetespkgapiv1Pod>
+            {
+                new Iok8skubernetespkgapiv1Pod { Metadata = new Iok8sapimachinerypkgapismetav1ObjectMeta { NamespaceProperty = PodNamespace, Name = InstanceId1 } }
+            };
+        }
+    }
+}
